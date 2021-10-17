e39fac6f0af2357d044316753890e45d6f46589a
amidh <amidp18@gmail.com>
2021-10-17 11:29:01 +0100

    I like git three times

---
+++ src/Newgit.java
@@ -0,0 +1,9 @@
+public class Newgit {
+    public static void main(String[] args) {
+        System.out.println("I like git");
+        System.out.println("I like git");
+        System.out.println("I like git");
+    }
+
+
+}
